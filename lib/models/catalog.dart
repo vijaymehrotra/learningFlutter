04188c20a2079f159411a3ca54728f2ee0c9a7a5
Vijay Mehrotra <vijaymehrotra99@gmail.com>
2023-08-29 19:38:39 +0530

    created catalog.dart

---
+++ lib/models/catalog.dart
@@ -0,0 +1,12 @@
+class MyCatalog {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  MyCatalog({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
+
+
