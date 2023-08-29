ac139b0b784d3e3a437ea7218d35114a703c6508
Vijay Mehrotra <vijaymehrotra99@gmail.com>
2023-08-30 01:54:18 +0530

    brought data from catalog.json and decoded it.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    // var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(50,(index) => CatalogModel.items[0],);
+    final dummyList = List.generate(
+      50,
+      (index) => CatalogModel.items[0],
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text(
