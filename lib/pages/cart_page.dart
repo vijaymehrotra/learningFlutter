e5561ca46da90f474711abd0ec9564df9b613ea0
Vijay Mehrotra <vijaymehrotra99@gmail.com>
2023-09-05 23:01:58 +0530

    cart page made correctoly day 17 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -36,7 +36,7 @@ class _CartTotal extends StatelessWidget {
           ElevatedButton(
             onPressed: () {
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: "Buying Not Supported Yet".text.make(),
+                content: "Buying Not Supported Yet".text.color(context.accentColor).make(),
               ));
             },
             style: ButtonStyle(
@@ -62,16 +62,23 @@ class __CartListState extends State<_CartList> {
   final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _cart.items.length,
-      itemBuilder: (context, index) => ListTile(
-        leading: Icon(Icons.done),
-        trailing: IconButton(
-          icon: Icon(Icons.remove_circle_outline),
-          onPressed: () {},
-        ),
-        title: _cart.items[index].name.text.make(),
-      ),
-    );
+    return _cart.items.isEmpty
+        ? "Nothing To Show".text.xl3.makeCentered()
+        : ListView.builder(
+            itemCount: _cart.items.length,
+            itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.done),
+              trailing: IconButton(
+                icon: Icon(Icons.remove_circle_outline),
+                onPressed: () {
+                  _cart.remove(_cart.items[index]);
+                  setState(() {
+                    
+                  });
+                },
+              ),
+              title: _cart.items[index].name.text.make(),
+            ),
+          );
   }
 }
--- lib/widgets/home_widgets/catalog_item.dart
+++ lib/widgets/home_widgets/catalog_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/models/cart_model.dart';
 import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widgets/home_widgets/add_to_cart.dart';
 import 'package:flutter_catalog/widgets/home_widgets/catalog_image.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -38,7 +39,9 @@ class CatalogItem extends StatelessWidget {
                         .color(context.theme.colorScheme.secondary)
                         .xl
                         .make(),
-                    _AddToCart(catalog: catalog,),
+                    AddToCart(
+                      catalog: catalog,
+                    ),
                   ],
                 ).pOnly(right: 8.0),
               ],
@@ -50,38 +53,4 @@ class CatalogItem extends StatelessWidget {
   }
 }
 
-class _AddToCart extends StatefulWidget {
-  final Item catalog;
-  const _AddToCart({
-    super.key,
-    required this.catalog,
-  });
 
-  @override
-  State<_AddToCart> createState() => _AddToCartState();
-}
-
-class _AddToCartState extends State<_AddToCart> {
-  bool isAdded = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        isAdded = isAdded.toggle();
-        final _catalog = CatalogModel();
-        final _cart = CartModel();
-        _cart.catalog = _catalog;
-        _cart.add(widget.catalog);
-        setState(() {});
-      },
-      style: ButtonStyle(
-          backgroundColor:
-              MaterialStateProperty.all(context.theme.highlightColor),
-          shape: MaterialStateProperty.all(
-            StadiumBorder(),
-          )),
-      child: isAdded ? Icon(Icons.done) : "Add to Cart".text.make(),
-    );
-  }
-}
