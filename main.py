856ad1899183fa483e5d475d828c9c593469bcba
Vijay Mehrotra <vijaymehrotra99@gmail.com>
2024-07-05 17:17:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("hello world!!")
 
 
 def welcome():
-  print("welcome to githib actions first time")
+    print("welcome to githib actions first time")  # Indent with 4 spaces
 
 
 print("This is tutorial for githib actions")
